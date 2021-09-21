241f76c419d3011ef97bb1102f3f2eb57b8c0e1b
A3939 <adityagpatel59@gmail.com>
2021-09-21 15:15:52 +0530

    otp sending sucessful

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,14 +1,38 @@
+const otpService = require('../services/otp-service')
+const hashService = require('../services/hash-service')
+
+
 class AuthController {
-    sendOtp(req, res) {
+    async sendOtp(req, res) {
         //Logic
         const { phone } = req.body;
         if (!phone) {
             res.status(400).json({ message: 'Phone Field is required!' })
         }
 
+        const otp = await otpService.generateOtp();
+
+        //Hash
+        const ttl = 1000 * 60 * 2; //2 min
+        const expires = Date.now() + ttl;
+        const data = `${phone}.${otp}.${expires}`;
+        const hash = hashService.hashOtp(data);
+
+        //send otp
+        try{
+
+            await otpService.sendBySms(phone,otp);
+            res.json({
+                hash: `${hash}.${expires}`,
+                phone,
+            })
+        }catch(err){
+            console.log(err)
+            res.status(500).json({message: 'message sending failed'})
+        }
 
 
-        res.send('Hello from Otp')
+        res.json({ hash: hash})
 
     }
 }
